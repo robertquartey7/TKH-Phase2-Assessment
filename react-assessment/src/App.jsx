2b8fa2ca3a82b0db8e49c0b39c0687fc51dfbc37
Robert Quartey <37972474+robertquartey7@users.noreply.github.com>
2023-03-27 22:13:31 -0400

    .

--- react-assessment/src/App.jsx
+++ react-assessment/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import useSWR from "swr";
 
+const fetcher = (url) => axios.get(url).then((res) => res.data);
 function App() {
-  const [count, setCount] = useState(0)
+  const { data, error, isLoading } = useSWR(
+    "https://pokeapi.co/api/v2/pokemon",
+    fetcher
+  );
+
+
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+      }}
+    >
+      {data && (
+        <div>
+          {data.results.map((element) => {
+            return (
+              <div
+                style={{
+                  border: "solid",
+                  padding: "1rem",
+                }}
+              >
+                <h1>{element.name}</h1>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
